9f99596894ed48a0bde76ebbf50e56a3638f5bf9
Guilherme Ventura <guilhermeventura2@gmail.com>
2014-01-18 19:43:22 -0200

    Improved Utils class.

--- AndroidUtils/src/com/ventura/androidutils/utils/Utils.java
+++ AndroidUtils/src/com/ventura/androidutils/utils/Utils.java
@@ -1,9 +1,16 @@
 package com.ventura.androidutils.utils;
 
+import java.io.File;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class Utils {
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+public final class Utils {
 	public static void CopyStream(InputStream is, OutputStream os) {
 		final int buffer_size = 1024;
 		try {
@@ -16,5 +23,35 @@ public class Utils {
 			}
 		} catch (Exception ex) {
 		}
+	}
+
+	public static String getFilePathFromContentUri(Uri fileUri,
+			ContentResolver contentResolver) {
+		// See http://stackoverflow.com/a/11603837
+		String filePath;
+		String[] filePathColumn = { MediaStore.MediaColumns.DATA };
+
+		Cursor cursor = contentResolver.query(fileUri, filePathColumn, null,
+				null, null);
+		cursor.moveToFirst();
+
+		int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+		filePath = cursor.getString(columnIndex);
+		cursor.close();
+		return filePath;
+	}
+
+	public static File getSharedFile(Context ctx, Uri path) {
+		if (path == null)
+			return null;
+
+		String uri;
+		if (path.toString().startsWith("content")) {
+			uri = Utils.getFilePathFromContentUri(path, ctx.getContentResolver());
+		} else {
+			uri = path.getPath();
+		}
+
+		return new File(uri);
 	}
 }
